a339637f3737afb3539c9d3a3f80157e7d6e4385
Lukasz <rejlukasz@gmail.com>
2021-02-15 18:01:52 +0100

    style: project cleanup, formatting etc.

--- src/pages/root/Root.tsx
+++ src/pages/root/Root.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../../styles/App.css';
 
-function App() {
+function Root() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -23,4 +20,4 @@ function App() {
   );
 }
 
-export default App;
+export default Root;
